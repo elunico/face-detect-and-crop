96351a14cf22c082b2b6612f6dca7417f0066627
Thomas Povinelli <tompov227@gmail.com>
2021-01-16 15:32:45 -0500

    Fixed limiting and added nowrite option

--- facedetect.py
+++ facedetect.py
@@ -19,6 +19,7 @@ def parse_args():
     ap.add_argument('-m', '--max', type=int, default=5, help='The maximum number of faces to extract. Defaults to 5. Use 0 for no limit')
     ap.add_argument('-b', '--box', action='store_true', help='Do not crop, but draw the bounding box to the photo and write that out')
     ap.add_argument('-s', '--show', action='store_true', help='Show each image as it is being output')
+    ap.add_argument('-n', '--nowrite', action='store_true', help='Perform transformations but do not write files. Implies -s.')
     return ap.parse_args()
 
 def get_name_and_extension(filename):
@@ -32,6 +33,16 @@ def get_name_and_extension(filename):
 def ensure_dir(dirname):
     if not os.path.isdir(dirname) and not os.path.exists(dirname):
         os.mkdir(dirname)
+
+def highest(value, limit):
+    if value > limit:
+        value = limit
+    return value
+
+def lowest(value, limit):
+    if value < limit:
+        value = limit
+    return value
 
 def bounding_boxes_for_id(filename: str, classifier: 'cv2.CascadeClassifier') -> List[Tuple[int, int, int, int]]:
     '''
@@ -62,10 +73,10 @@ def bounding_boxes_for_id(filename: str, classifier: 'cv2.CascadeClassifier') ->
             # continue
         x1, y1, x2, y2 = x, y, x + width, y + height
         x1, y1, x2, y2 = x1 - (width//3), y1 - (height//2), x2 + (width//3), y2 + (height//2)
-        if x1 < 0: x1 = 0
-        if y1 < 0: y1 = 0
-        if x2 >= pixels.shape[1]: x2 = pixels.shape[1] - 1
-        if y2 >= pixels.shape[0]: y2 = pixels.shape[0] - 1
+        x1 = lowest(x1, 0)
+        y1 = lowest(y1, 0)
+        x2 = highest(x2, pixels.shape[1] - 1)
+        y2 = highest(y2, pixels.shape[0] - 1)
         faces.append((x1, y1, x2, y2))
     return faces
 
@@ -75,7 +86,8 @@ def _on_each_box(
             boxes: List[Tuple[int, int, int, int]],
             outdir: str,
             transform: Callable[['cv2.image', int, int, int, int], 'cv2.image'],
-            show: bool = False
+            show: bool = False,
+            write: bool = True
         ) -> 'List[cv2.image]':
     image = cv2.imread(name + ext)
     results = []
@@ -87,14 +99,15 @@ def _on_each_box(
             cv2.imshow('Result', result)
             cv2.waitKey(0)
         outfile = '{}/{}_face{}{}'.format(outdir, name, i, ext)
-        try:
-            cv2.imwrite(outfile, result)
-        except cv2.error as e:
-            print("Could not write image called {}".format(outfile))
-            print(e.msg)
+        if write:
+            try:
+                cv2.imwrite(outfile, result)
+            except cv2.error as e:
+                print("Could not write image called {}".format(outfile))
+                print(e.msg)
     return results
 
-def crop_to_boxes(filename: str, boxes: List[Tuple[int, int, int, int]], show: bool = False) -> 'List[cv2.image]':
+def crop_to_boxes(filename: str, boxes: List[Tuple[int, int, int, int]], show: bool = False, write: bool = True) -> 'List[cv2.image]':
     '''
     This function takes a filename of an image and a list of bounding boxes
     to crop to. It crops the image called filename to the places specified
@@ -108,11 +121,12 @@ def crop_to_boxes(filename: str, boxes: List[Tuple[int, int, int, int]], show: b
     list of bounding box
     '''
     name, ext = get_name_and_extension(filename)
-    ensure_dir('cropped')
+    if write:
+        ensure_dir('cropped')
 
-    _on_each_box(name, ext, boxes, 'cropped', lambda img, x1, y1, x2, y2: img[y1:y2, x1:x2], show)
+    _on_each_box(name, ext, boxes, 'cropped', lambda img, x1, y1, x2, y2: img[y1:y2, x1:x2], show, write)
 
-def draw_bounding_box(filename: str, boxes: List[Tuple[int, int, int, int]], show: bool = False) -> 'List[cv2.image]':
+def draw_bounding_box(filename: str, boxes: List[Tuple[int, int, int, int]], show: bool = False, write: bool = True) -> 'List[cv2.image]':
     '''
     This function takes a filename of an image and a list of bounding boxes
     It draws a rectangle around the given bounding box and writes the image out
@@ -125,14 +139,15 @@ def draw_bounding_box(filename: str, boxes: List[Tuple[int, int, int, int]], sho
     list of bounding box
     '''
     name, ext = get_name_and_extension(filename)
-    ensure_dir('marked')
+    if write:
+        ensure_dir('marked')
 
     def draw_rect(img, x1, y1, x2, y2, color=(0, 0, 255)):
         image = img.copy()
         cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
         return image
 
-    _on_each_box(name, ext, boxes, 'marked', draw_rect, show)
+    _on_each_box(name, ext, boxes, 'marked', draw_rect, show, write)
 
 
 def test():
@@ -141,14 +156,15 @@ def test():
         boxes = bounding_boxes_for_id(name, classifier)
         draw_bounding_box(name, boxes)
 
-def main_for_file(filename, drawOnly: bool = False, show: bool = False, limit: int = 5):
+def main_for_file(filename, drawOnly: bool = False, show: bool = False, limit: int = 5, write: bool = True):
     boxes = bounding_boxes_for_id(filename, classifier)
     if len(boxes) > limit:
         print("Warning: file {} -> limit was {} but found {} faces. Taking first {}".format(filename, limit, len(boxes), limit))
+        boxes = boxes[:limit]
     if drawOnly:
-        results = draw_bounding_box(filename, boxes, show)
+        results = draw_bounding_box(filename, boxes, show, write)
     else:
-        results = crop_to_boxes(filename, boxes, show)
+        results = crop_to_boxes(filename, boxes, show, write)
     return results
 
 def main():
@@ -158,9 +174,9 @@ def main():
         os.chdir(options.directory)
         for filename in os.listdir('.'):
             if not os.path.isdir(filename):
-                main_for_file(filename, options.box, options.show, options.max)
+                main_for_file(filename, options.box, options.show or options.nowrite, options.max, not options.nowrite)
     elif options.file:
-        main_for_file(options.file, options.box, options.show, options.max)
+        main_for_file(options.file, options.box, options.show or options.nowrite, options.max, not options.nowrite)
 
 
 
